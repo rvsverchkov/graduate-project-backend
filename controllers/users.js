68ed198f7198344b5dec14b18ef02988c9da2dbf
rvsverchkov <s10z40@yandex.ru>
2020-12-20 00:42:51 +0300

    Diploma Project - Authorization and registation

--- controllers/users.js
+++ controllers/users.js
@@ -16,9 +16,23 @@ const registerUser = (req, res, next) => {
             name
         }))
         .then((user) => res.send(user))
-        .catch((error) => res.send(error))
+        .catch((error) => res.send(error));
+}
+
+const loginUser = (req, res, next) => {
+    const { email, password } = req.body;
+    return User.findUserByCredentials(email, password)
+        .then((user) => {
+            const token = jwt.sign(
+                { _id: user._id },
+                NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+                { expiresIn: '7d' },
+            );
+            res.send({ token });
+        })
+        .catch((error) => res.send(error));
 }
 
 module.exports = {
-    registerUser
+    registerUser, loginUser
 };
